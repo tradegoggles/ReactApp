32eff5855f5d2fde7f35ae0ccbba72fc02d069e7
Jeff Carlson <jeffcarlson@mail.com>
2019-10-23 02:59:01 -0500

    Added React

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -4,7 +4,7 @@ import './App.css';
 
 const App: React.FC = () => {
   return (
-    <div className="App">+
+    <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
